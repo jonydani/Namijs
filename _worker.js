f20cf51a81ae96b93d860a726431aec7db49b895
jonydani <67456454+jonydani@users.noreply.github.com>
2025-06-12 17:44:20 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,8 +3,8 @@ import { connect } from "cloudflare:sockets";
 // import { Buffer } from "node:buffer";
 
 // Variables
-const rootDomain = "namijs.pages.dev"; // Ganti dengan domain utama kalian
-const serviceName = "f91f41ae"; // Ganti dengan nama workers kalian
+const rootDomain = "pages.dev"; // Ganti dengan domain utama kalian
+const serviceName = "namijs"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
